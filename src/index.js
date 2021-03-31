6a2532ce26c99b925c1a5561cb6a1efedccba5b1
V <victor.hugo.pacheco.flores@gmail.com>
2021-03-31 17:26:11 -0600

    Create the page with img, title and nav menu

--- src/index.js
+++ src/index.js
@@ -8,19 +8,55 @@ function component() {
   img.classList.add('rounded');
   img.id = 'first-img';
   img.src = banner;
-  addRowCol(img);
+  addRowCol(img, 'content');
 
   const title = document.createElement('h1');
   title.innerHTML = 'Italian restaurant';
-  addRowCol(title);
+  addRowCol(title, 'content');
 
   const copy = document.createElement('p');
   copy.innerHTML = 'All kind of Italian food dishes and more...';
-  addRowCol(copy);
+  addRowCol(copy, 'content');
+
+  const ul = document.createElement('ul');
+  ul.classList.add('nav');
+  ul.classList.add('nav-pills');
+  ul.classList.add('nav-fill');
+  addLi(ul, 'About us', 'about', true);
+  addLi(ul, 'Our menu', 'menu');
+  addLi(ul, 'Location', 'location');
+  const navigation = document.createElement('div');
+  navigation.id = 'navigation';
+  document.body.appendChild(navigation);
+  navigation.appendChild(ul);
+
+  const text = document.createElement('div');
+  text.id = 'text';
+  document.body.appendChild(text);
+
+  changeText('about');
 }
 
-function addRowCol(obj) {
-  const content = document.getElementById('content');  
+function changeText(tag) {
+  const text = document.getElementById('text');
+  text.innerHTML = '';
+  const p = document.createElement('p');
+  switch (tag) {
+      case 'about':
+        p.innerHTML = ' We\'re an Italian restaurante since 1955';
+        break;
+      case 'menu':
+        p.innerHTML = 'Pasta, meat, vine and more...';
+        break;
+      case 'location':
+        p.innerHTML = 'We\'re on Bakersfield street, number 221b';
+        break;
+  }
+  addRowCol(p, 'text');
+}
+
+function addRowCol(obj, c) {
+  const content = document.getElementById(c);  
   const row = document.createElement('div');
   row.classList.add('row');
   row.classList.add('text-center');
@@ -29,6 +65,39 @@ function addRowCol(obj) {
   row.appendChild(col);
   col.appendChild(obj);
   content.appendChild(row);
+}
+
+function addLi(ul, str, tag, status = false){
+  const li = document.createElement('li');
+  li.classList.add('nav-item');
+  const a = document.createElement('a');
+  a.classList.add('nav-link');
+  a.classList.add(tag);
+  (status == true) ? a.classList.add('active'):'';
+  (status == true) ? a.id = 'a-green' : '';
+  a.addEventListener('click', () => { changeNav(tag)});
+  a.onclick = 'this()';
+  a.innerHTML = str;
+  li.appendChild(a)
+  ul.appendChild(li);
+}
+
+function changeNav(tag) {
+  const arr = document.getElementsByClassName('nav-link');
+  
+  for (let i = 0; i < arr.length; i += 1) {
+    arr[i].id = '';
+    arr[i].classList.remove('active');
+    let clas = 'nav-link ' + tag;
+    if (arr[i].className == clas) {
+      arr[i].id = 'a-green';
+      arr[i].classList.add('active');
+      changeText(tag);
+    }
+  }
+  //const a = document.getElementsByClassName(`${tag}`);
+  //a.id = 'a-green';
+  //a.classList.add('active');
 }
 
 component();
